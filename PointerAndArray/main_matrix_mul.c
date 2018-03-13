1f1e84336822a25fd925810d56d3677dfadbefbc
Milo Chen <milo@pieapple.com>
2018-03-13 23:15:04 +0800

    refactor

--- PointerAndArray/main_matrix_mul.c
+++ PointerAndArray/main_matrix_mul.c
@@ -21,27 +21,34 @@ void matrix_set(int rows, int cols, int* matrix, int i, int j, int val);
 //For the matrix with size = rows x cols, we set val into the (i,j) position in matrix.
 
 void matrix_set_zeros(int rows,int cols,int * matrix);
-
-
+//For the matrix with size = rows x cols, we set zero in every cell.
 
 void matrix_size_input_procedure(int *pm, int *pn, int *pp);
 //The purpose is to figure matrix C = A * B
 //assistant procedure to setup the size information about matrix A and matrix B
 
 void matrix_cell_input_procedure(int rows, int cols, int *matrix, char* matrix_name);
+//help you to input all cell data after you call this function
 
 void print_matrix(int rows, int cols, int *matrix, char* matrix_name);
-
-//void matrix_multiplication(int *C,int *A,int *B,int m,int n,int p);
-//matrix C = matrix A * matrix B when A is (m,n) matrix and B is (n,p) matrix.
-
+//print matrix data.
 
 
 int main() {
-	int m, n, p;
+	/*
+		Our purpose is do C_mp = A_mn * B_np
+		C is matrix with m * p size
+		A is matrix with m * n size
+		B is matrix with n * p size
+
+	*/
+
+	int m, n, p; 
 	int *C;//matrix C 
 	int *A;//matrix A
 	int *B;//matrix B
+
+	//input the size of m , n &p 
 	matrix_size_input_procedure(&m,&n,&p);
 
 	//dynamic allocate the memory space for the matrix
@@ -49,12 +56,16 @@ int main() {
 	A=(int*)malloc(sizeof(int)*(m*n));
 	B=(int*)malloc(sizeof(int)*(n*p));
 
+	//Let user to input matrix A 
 	matrix_cell_input_procedure(m,n,A, "A");
+
+	//Let user to input matrix B
 	matrix_cell_input_procedure(n,p,B, "B");
 	
+	//set all cell zero in matrix C.
 	matrix_set_zeros(m,p,C);
 
-	//matrix_multiplication(C,A,B,m,n,p) as the following
+	//Do the matrix multiplication : C_mp = A_mn * B_np
 	int i,j,k;
 	for (i=0;i<m;i++) {
 		for(j=0;j<p;j++){
@@ -63,13 +74,14 @@ int main() {
 				int A_ik = matrix_get(m,n,A,i,k);
 				int B_kj = matrix_get(n,p,B,k,j);
 				C_ij = C_ij + A_ik * B_kj;
-
-				printf("A_%d%d=%d,\tB_%d%d=%d,\tC_%d%d = %d\n",  i,k,A_ik,  k,j,B_kj,  i,j, C_ij);
+				/* The following code is just help you to debug */
+				//printf("A_%d%d=%d,\tB_%d%d=%d,\tC_%d%d = %d\n",  i,k,A_ik,  k,j,B_kj,  i,j, C_ij);
 			}
 
 			matrix_set(m,p,C,i,j,C_ij);
 		}
 	}
+	//print the result of matrix C
 	print_matrix(m,p,C,"C");
 
 
@@ -77,7 +89,7 @@ int main() {
 	//release the memory space
 	free(C);
 	free(B);
-	free(A);
+	free(A); //there is some issue when release A matrix...
 
 	return 0;
 }
