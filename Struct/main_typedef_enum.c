962c835b385da75311d210d89563af7b92c62a24
Milo Chen <milo@pieapple.com>
2018-03-19 14:32:38 +0800

    update

--- Struct/main_typedef_enum.c
+++ Struct/main_typedef_enum.c
@@ -1,23 +1,4 @@
-/* 
-typedef and struct
-struct book{ float value};
-struct book mybook;
-struct book* ptr;
-typedef struct book BOOKS; //BOOKS mybook;
-typedef BOOKS* pBooks; //=> pBooks ptr;
 
-typedef struct BookStore {
-	float value;
-} BOOKS_STORE;
-
-
-enum spectrum {RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET};
-enum BOOL {FALSE, TRUE};
-enum spectrum color;
-int color = BLUE;
-
-
-*/
 
 #include <stdio.h> 
 
