fd62f8076498e35997d82fda4aab6c7fb4cd9757
Milo Chen <milo@pieapple.com>
2018-04-03 11:21:27 +0800

    commit

--- SingalAndProcess/generateNprocess.c
+++ SingalAndProcess/generateNprocess.c
@@ -8,32 +8,32 @@
 */
 int main(int argc, char *argv[])
 {
-    pid_t pid;
-    char message[100];
-
-	 pid_t pids[100];
-    int n;
+	char message[100];
+	pid_t pids[100];
+	int n;
 	
+	pid_t root_pid = getpid();
 	
-   printf("start: ppid=%d \t pid=%d \n", getppid(), getpid());
+	printf("start: ppid=%d \t pid=%d \n", getppid(), getpid());
 	if(argc>1) {
 		n = atoi(argv[1]);
 		int i;	
 		for ( i = 0; i < n ; i++ ) {
 			int new_pid = fork();
-			if(new_pid == 0) { 
+			if(new_pid == 0) { //case of in New process
 				//this is code for new process
    			//printf("child start: ppid=%d \t pid=%d \n", getppid(), getpid());
 				break;
 			}
 			else {
-				if(new_pid > 0) {
+				if(new_pid > 0) { //case of in Original root process
 					pids[i] = new_pid;
-   				printf("ppid=%d \t pid=%d \n", getpid(), pids[i]);
+					printf("ppid=%d \t pid=%d \n", getpid(), pids[i]);
 				}
 			}
 		}
 	}
+	
 
 	exit(0);
 }
